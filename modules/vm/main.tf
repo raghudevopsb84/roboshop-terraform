3b2883b973f24f928eab310f919f33f47a677e91
Raghu K <raghuk.vit@gmail.com>
2025-05-05 06:40:58 +0530

    RoboShop Project

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -66,9 +66,10 @@ resource "null_resource" "ansible" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo dnf install python3.12 python3.12-pip -y",
-      "sudo pip3.12 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/raghudevopsb84/roboshop-ansible roboshop.yml -e app_name=${var.name} -e env=dev"
+      "echo OK"
+#       "sudo dnf install python3.12 python3.12-pip -y",
+#       "sudo pip3.12 install ansible",
+#       "ansible-pull -i localhost, -U https://github.com/raghudevopsb84/roboshop-ansible roboshop.yml -e app_name=${var.name} -e env=dev"
     ]
   }
 }
