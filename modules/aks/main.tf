ffcd6424e179c70f797dc0038d9117070a1a6d05
Raghu K <raghuk.vit@gmail.com>
2025-05-20 07:04:08 +0530

    RoboShop Project

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_kubernetes_cluster" "main" {
     subnet_name = "/subscriptions/323379f3-3beb-4865-821e-0fff68e4d4ca/resourceGroups/project-setup-1/providers/Microsoft.Network/virtualNetworks/main/subnets/default"
   }
 
-  network_plugin {
+  network_profile {
     network_plugin = "azure"
     pod_cidr = "10.100.0.0/24"
   }
