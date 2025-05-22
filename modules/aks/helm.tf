e508109a36ae316f7d0d9b56d5906972bfdb8d90
Raghu K <raghuk.vit@gmail.com>
2025-05-22 06:15:47 +0530

    RoboShop Project

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -1,4 +1,14 @@
+resource "null_resource" "kubeconfig" {
+  provisioner "local-exec" {
+    command = "az aks get-credentials --name ${var.name} --resource-group ${var.rg_name} --overwrite-existing"
+  }
+}
+
 resource "helm_release" "external-secrets" {
+  depends_on = [
+    null_resource.kubeconfig
+  ]
+
   name             = "external-secrets"
   repository       = "https://charts.external-secrets.io"
   chart            = "external-secrets"
