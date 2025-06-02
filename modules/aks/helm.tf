4b7f2f2a57943bf87604f7dbc1823b5d8c04efeb
Raghu K <raghuk.vit@gmail.com>
2025-06-02 08:02:24 +0530

    RoboShop Project

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -60,3 +60,17 @@ TF
   }
 }
 
+
+resource "helm_release" "istio-base" {
+  depends_on = [
+    null_resource.kubeconfig
+  ]
+
+  name             = "istio-base"
+  repository       = "https://istio-release.storage.googleapis.com/charts"
+  chart            = "base"
+  namespace        = "istio-system"
+  create_namespace = true
+}
+
+
