a67e96a3046597fae98dd81ce8008a2626a6ab0c
Raghu K <raghuk.vit@gmail.com>
2025-06-23 06:29:30 +0530

    RoboShop Project

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -16,22 +16,22 @@ EOF
   }
 }
 
-# resource "helm_release" "external-secrets" {
-#   depends_on = [
-#     null_resource.kubeconfig
-#   ]
-#
-#   name             = "external-secrets"
-#   repository       = "https://charts.external-secrets.io"
-#   chart            = "external-secrets"
-#   namespace        = "devops"
-#   create_namespace = true
-#   set {
-#     name  = "installCRDs"
-#     value = "true"
-#   }
-# }
-#
+resource "helm_release" "external-secrets" {
+  depends_on = [
+    null_resource.kubeconfig
+  ]
+
+  name             = "external-secrets"
+  repository       = "https://charts.external-secrets.io"
+  chart            = "external-secrets"
+  namespace        = "devops"
+  create_namespace = true
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
+
 # resource "null_resource" "external-secrets-secret-store" {
 #   depends_on = [
 #     helm_release.external-secrets
