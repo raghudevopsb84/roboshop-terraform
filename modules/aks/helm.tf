5387fa5bebc47135f5949807e8db1e2c89678020
Raghu K <raghuk.vit@gmail.com>
2025-06-23 06:28:11 +0530

    RoboShop Project

---
+++ modules/aks/helm.tf
@@ -0,0 +1,69 @@
+resource "null_resource" "kubeconfig" {
+  depends_on = [
+    azurerm_kubernetes_cluster.main,
+    azurerm_kubernetes_cluster_node_pool.main
+  ]
+
+  triggers = {
+    time = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
+az aks get-credentials --name ${var.name} --resource-group ${var.rg_name} --overwrite-existing
+EOF
+  }
+}
+
+# resource "helm_release" "external-secrets" {
+#   depends_on = [
+#     null_resource.kubeconfig
+#   ]
+#
+#   name             = "external-secrets"
+#   repository       = "https://charts.external-secrets.io"
+#   chart            = "external-secrets"
+#   namespace        = "devops"
+#   create_namespace = true
+#   set {
+#     name  = "installCRDs"
+#     value = "true"
+#   }
+# }
+#
+# resource "null_resource" "external-secrets-secret-store" {
+#   depends_on = [
+#     helm_release.external-secrets
+#   ]
+#
+#   provisioner "local-exec" {
+#     command = <<TF
+# kubectl apply -f - <<KUBE
+# apiVersion: external-secrets.io/v1
+# kind: ClusterSecretStore
+# metadata:
+#   name: roboshop-${var.env}
+# spec:
+#   provider:
+#     vault:
+#       server: "http://vault-int.rdevopsb84.online:8200"
+#       path: "roboshop-${var.env}"
+#       version: "v2"
+#       auth:
+#         tokenSecretRef:
+#           name: "vault-token"
+#           key: "token"
+#           namespace: devops
+# ---
+# apiVersion: v1
+# kind: Secret
+# metadata:
+#   name: vault-token
+#   namespace: devops
+# data:
+#   token: ${base64encode(var.token)}
+# KUBE
+# TF
+#   }
+# }
