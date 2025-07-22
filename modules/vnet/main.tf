25331ee9f1f1509a456be133c5dfbb7fa5a9d5e3
Raghu K <raghuk.vit@gmail.com>
2025-07-22 06:52:48 +0530

    RoboShop Project

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_network" "main" {
 
 resource "azurerm_subnet" "main" {
   for_each             = var.subnets
-  name                 = "${each.name}-subnet"
+  name                 = "${each.key}-subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = each.value["address_prefixes"]
