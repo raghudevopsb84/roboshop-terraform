c425695a250fd8c65db1215316f7835cb0e285bb
Raghu K <raghuk.vit@gmail.com>
2025-07-22 07:31:17 +0530

    RoboShop Project

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_subnet" "main" {
 }
 
 resource "azurerm_virtual_network_peering" "here-to-tools" {
-  name                      = "${var.name}-${var.env}-conn-to-tools"
+  name                      = "${var.name}-conn-to-tools"
   resource_group_name       = var.resource_group_name
   virtual_network_name      = azurerm_virtual_network.main.name
   remote_virtual_network_id = var.tools_vnet_resource_id
@@ -22,9 +22,9 @@ resource "azurerm_virtual_network_peering" "here-to-tools" {
 
 
 resource "azurerm_virtual_network_peering" "tools-to-here" {
-  name                      = "tools-conn-to-${var.name}-${var.env}"
+  name                      = "tools-conn-to-${var.name}"
   resource_group_name       = var.resource_group_name
-  virtual_network_name      = var.tools_vnet_resource_id
+  virtual_network_name      = split("/", var.tools_vnet_resource_id)[8]
   remote_virtual_network_id = azurerm_virtual_network.main.id
 }
 
