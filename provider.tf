9848af292688eafffdac3a7c0e62c7c437674129
Raghu K <raghuk.vit@gmail.com>
2025-08-19 06:01:41 +0530

    RoboShop Project

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {}
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "4.8.0"
+    }
+  }
 }
 
 provider "vault" {
