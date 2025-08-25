83695227374b713db8818090bfb297af0253c027
Raghu K <raghuk.vit@gmail.com>
2025-08-25 07:09:59 +0530

    RoboShop Project

--- main.tf
+++ main.tf
@@ -74,12 +74,14 @@ module "aks" {
   vnet_subnet_id    = module.vnet["${each.value["vnet_prefix"]}-${var.env}"].subnet[each.value["subnet"]].id
 }
 
-module "mysql" {
-  for_each = var.mysql
-  source = "./modules/mysql"
-  rg_location = module.resource-group[each.value["rgname"]].location
-  rg_name = module.resource-group[each.value["rgname"]].name
-  vnet_id = module.vnet["${each.value["vnet_prefix"]}-${var.env}"].vnet_id[each.value["subnet"]].id
+# module "mysql" {
+#   for_each = var.mysql
+#   source = "./modules/mysql"
+#   rg_location = module.resource-group[each.value["rgname"]].location
+#   rg_name = module.resource-group[each.value["rgname"]].name
+#   vnet_id = module.vnet["${each.value["vnet_prefix"]}-${var.env}"].vnet_id[each.value["subnet"]].id
+# }
+
+output "main" {
+  value = module.vnet
 }
-
-
